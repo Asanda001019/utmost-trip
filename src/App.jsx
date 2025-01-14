f93f99e84d6fde20dabf27050063ec3ca14563d0
User <User@WRSD-LD309-018.dut.ac.za>
2025-01-14 11:52:47 +0200

    feat: Login form ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import Weather from './pages/Weather'
-import Favorites from './pages/Favorites'
-import NoPage from './pages/NoPage' 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Weather from './pages/Weather';
+import Favorites from './pages/Favorites';
+import NoPage from './pages/NoPage';
 
-import './App.css'
+import './App.css';
 
 function App() {
- 
-
   return (
-    <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="weather" element={<Weather />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="weather" element={<Weather />} />
+        <Route path="favorites" element={<Favorites />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
-       
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
